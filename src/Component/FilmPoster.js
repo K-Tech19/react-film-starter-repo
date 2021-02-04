c7e4c7c1daeeee7e18d7989a053941e378b594cd
Kenny Jean <kennyjean561@gmail.com>
2021-02-04 11:52:20 -0500

    assignment complete

---
+++ src/Component/FilmPoster.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+
+class FilmPoster extends Component {
+
+
+    render(){
+
+        return(
+
+            <img src={`https://image.tmdb.org/t/p/w780/${this.props.poster_path}`} alt={this.props.title} />
+    
+        )
+    }
+}
+export default FilmPoster
